3910dad4710b91ea983eb7362f56d785b9aa91e5
JC Lucas <jclucasasp@gmail.com>
2024-08-28 15:20:27 +0200

    Bug fixes and refactoring according to db changes

--- src/main/java/com/example/tinder_ai_backend/config/WebSecurityConfig.java
+++ src/main/java/com/example/tinder_ai_backend/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.tinder_ai_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,11 +13,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
