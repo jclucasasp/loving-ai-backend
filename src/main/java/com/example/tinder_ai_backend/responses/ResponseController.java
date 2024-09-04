76b826e7afa101a94d3f8ff3fe5f1ad06da54f75
JC Lucas <jclucasasp@gmail.com>
2024-09-04 15:50:37 +0200

    Improved the model responses and personality

--- src/main/java/com/example/tinder_ai_backend/responses/ResponseController.java
+++ src/main/java/com/example/tinder_ai_backend/responses/ResponseController.java
@@ -1,35 +1,29 @@
 package com.example.tinder_ai_backend.responses;
 
+import com.example.tinder_ai_backend.config.ChatClientConfig;
 import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
+
 
 @CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 public class ResponseController {
 
-    private final static Logger logger =  LogManager.getLogger(ResponseController.class);
+    private final static Logger logger = LogManager.getLogger(ResponseController.class);
     private final ResponseService service;
+    private final ChatClientConfig config;
 
     @GetMapping(path = "/response")
-    public ResponseEntity<String> getResponse(@RequestBody Response req) {
-        Future<String> future = service.generateChatResponse(new Prompt(req.messagePrompt()));
-        try {
-            return  ResponseEntity.ok(future.get());
-        } catch (Exception e) {
-            logger.error(e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<String> getResponse(@RequestBody Response req) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(service.generateChatResponse(req.messagePrompt()).get());
     }
 }
