5960fa02b7e20bfe38661ac18244603933b83ebf
JC Lucas <jclucasasp@gmail.com>
2024-09-08 14:26:07 +0200

    Set up a scheduled service in the ExecutorConfig class to dynamically update the thread pool size if necessary

--- src/main/java/com/example/tinder_ai_backend/config/ExecutorConfig.java
+++ src/main/java/com/example/tinder_ai_backend/config/ExecutorConfig.java
@@ -1,12 +1,21 @@
 package com.example.tinder_ai_backend.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class ExecutorConfig {
+
+    private final Logger logger = LogManager.getLogger(ExecutorConfig.class);
+
     @Value("${spring.task.execution.pool.core-size}")
     private int coreSize;
 
@@ -21,11 +30,23 @@ public class ExecutorConfig {
 
     @Bean
     public ThreadPoolTaskExecutor chatResponseExecutor() {
+
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(coreSize);
         executor.setMaxPoolSize(maxPool);
         executor.setKeepAliveSeconds(keepAliveSeconds);
         executor.setQueueCapacity(queueCap);
+
+        try (ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor()) {
+            scheduler.scheduleAtFixedRate(() -> {
+                if (executor.getQueueSize() == queueCap) {
+                    logger.warn("");
+                    executor.setCorePoolSize(executor.getCorePoolSize() * 2);
+                    executor.setMaxPoolSize(executor.getMaxPoolSize() * 2);
+                }
+            }, 1, 10, TimeUnit.SECONDS);
+        }
+
         return executor;
     }
 }
