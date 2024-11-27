518d77abe6fadffb397cb556b3a4ac8ef49d1c47
JC <jclucasasp@gmail.com>
2024-11-27 16:12:14 +0200

    first commit of new app

--- src/main/java/ai/memory/ai/chat/memory/Application.java
+++ src/main/java/ai/memory/ai/chat/memory/Application.java
@@ -1,7 +1,6 @@
 package ai.memory.ai.chat.memory;
 
 import ai.memory.ai.chat.memory.services.DataBaseService;
-import ai.memory.ai.chat.memory.services.SystemPromptFileReader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
