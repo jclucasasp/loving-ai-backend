c1935528512c5b768f7a0b1d7a165810d221041b
jc <jclucasasp@gmail.com>
2024-12-04 18:59:24 +0200

    Added the users profile to the ai for better conversation

--- src/main/java/ai/memory/ai/chat/memory/services/PromptGenerator.java
+++ src/main/java/ai/memory/ai/chat/memory/services/PromptGenerator.java
@@ -1,5 +1,8 @@
 package ai.memory.ai.chat.memory.services;
 
+import ai.memory.ai.chat.memory.personalities.PersonalityDescriptionRepo;
+import ai.memory.ai.chat.memory.profile.Profile;
+import ai.memory.ai.chat.memory.profile.ProfileRepo;
 import ai.memory.ai.chat.memory.responses.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +14,10 @@ import java.util.Map;
 public class PromptGenerator {
 
     private final SystemPromptFileReader reader;
+    private final ProfileRepo profileRepo;
+    private final PersonalityDescriptionRepo personalityDescriptionRepo;
 
-    public Map<String, String> generatedChatPrompt(Response res) {
+    Map<String, String> generatedChatPrompt(Response res) {
         StringBuilder extraConfigBuilder = new StringBuilder();
         String name = res.name();
         int age = res.age();
@@ -26,6 +31,8 @@ public class PromptGenerator {
             DEFAULT_SYSTEM = "";
         }
 
+        Profile userProfile = profileRepo.getProfileByUserId(res.userId()).orElseThrow();
+
         extraConfigBuilder.append(DEFAULT_SYSTEM)
                 .append("Your Persona: ").append(age).append("-year-old single ")
                 .append(ethnicity).append(" ").append(gender)
@@ -35,6 +42,9 @@ public class PromptGenerator {
                 .append("\nYour responses will be based off your gender, ethnicity, personality type, bio and age.")
                 .append("\nYou do have a profile picture on this dating site.")
                 .append("\nYou would be chatting with the opposite of your gender.")
+                .append("\nThe person you are talking to is called ").append(userProfile.getFirstName().concat(" ").concat(userProfile.getLastName()))
+                .append("\nTheir bio is ").append(userProfile.getBio())
+                .append("\nTheir personality type is " + personalityDescriptionRepo.findById(userProfile.getMyersBriggsPersonalityType()))
                 .append("\nIt is okay for you to go off script every now and again.");
 
         String extraConfig = extraConfigBuilder.toString();
